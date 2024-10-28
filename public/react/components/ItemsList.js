e4baced7aa6aa88adff51cd58bfb11971e5231ed
Dhamu2955 <Dharmendra.Pindolia@gmail.com>
2024-10-28 15:49:17 +0000

    Fixed front end

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Item } from './Item';
 
-export const ItemsList = ({ items }) => {
+export const ItemsList = ({ items = [] }) => {
     return (
         <>
             {items.map((item, idx) => (
-                <div key={idx} >
+                <div key={idx}>
                     <Item item={item} />
                 </div>
             ))}
