fee3a37f8e37a10f5da2adbab35ca0b9583bc53a
mh2011-maisie <mh2011.music@gmail.com>
2024-10-29 11:53:15 +0000

    Add car method

--- server/routes/items.js
+++ server/routes/items.js
@@ -10,5 +10,19 @@ router.get("/", async (req, res, next) => {
       next(error);
     }
   });
+
+  router.post("/", async (req, res, next) => {
+    try {
+      // Destructure car details from the request body
+      const {color, year, mileage, make, model, bhp, raaminess, description,image, price} = req.body;
   
+      // Create a new item entry
+      const newItem = await Items.create({color,year,mileage,make,model,bhp,raaminess,description,image,price});
+  
+      // Respond with the newly created item
+      res.status(201).json(newItem);
+    } catch (error) {
+      next(error);
+    }
+  });
   module.exports = router;
