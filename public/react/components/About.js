7b67047874359d3eac25f2fa8a7b9aa3c5368ffc
mh2011-maisie <mh2011.music@gmail.com>
2024-10-31 16:01:08 +0000

    Styles

--- public/react/components/About.js
+++ public/react/components/About.js
@@ -6,7 +6,7 @@ export const About = () => {
         <div className="about-page">
             <h2 className="about-title">About Us</h2>
             <p className="about-description">
-                Yo! Welcome to **RAAMI WHIPS**, the baddest place for all things speed and style, yaara. We're talkin' top-notch motors that look fresh and run smooth. Cuzzeh, we’re all about that active life, bringing you rides that’ll make heads turn, kasssmeh! Ain't no one doing it like us in the ends. From the sleekest rides to the maddest upgrades, if you’re lookin' for that next-level whip, you found it here. RAAMI WHIPS – where style meets street, every single time.
+                 Welcome to <strong>RAAMI WHIPS</strong>, the baddest place for all things speed and style, yaara. We're talkin' top-notch motors that look fresh and run smooth. Cuzzeh, we’re all about that active life, bringing you rides that’ll make heads turn, kasssmeh! Ain't no one doing it like us in the ends. From the sleekest rides to the maddest upgrades, if you’re lookin' for that next-level whip, you found it here. RAAMI WHIPS – where style meets street, every single time.
             </p>
         </div>
     );
